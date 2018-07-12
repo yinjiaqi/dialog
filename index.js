3b7800e6bc4bd19e52de0583cc67b6a36877150f
anovasia <40883517+anovasia@users.noreply.github.com>
2018-07-12 17:10:56 +1200

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,5 @@
 "use strict";
-var request = require('request');
-var url = 'https://jsonplaceholder.typicode.com/posts/1';
-request.get({
-    url: url,
-    json: true,
-    headers: {'User-Agent': 'request'}
-  }, (err, res, data) => {
-    if (err) {
-      console.log('Error:', err);
-    } else if (res.statusCode !== 200) {
-      console.log('Status:', res.statusCode);
-    } else {
-      // data is already parsed as JSON:
-      console.log(data);
-    }
-});
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
