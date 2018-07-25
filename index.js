2c761680083fc93fae55a958599676426c89fa8b
yinjiaqi <40558566+yinjiaqi@users.noreply.github.com>
2018-07-26 00:28:04 +0800

    Update index.js

--- index.js
+++ index.js
@@ -63,10 +63,10 @@ database: "sql12247448"
   con.query("SELECT * FROM Employee where eid="+qstring, function (err, result) {
     if (err) throw err;
    tableify([{
-  eid: 'result.eid',
-  fname: 'result.fname',
-  lname: 'result.lname',
-  salary: 'result.salary'
+  eid: 'result[0].eid',
+  fname: 'result[0].fname',
+  lname: 'result[0].lname',
+  salary: 'result[0].salary'
 }], {
   headers: [{
     name: 'eid',
