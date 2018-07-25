45387ad78be12c475ef6a331b0b2e33fc4a5ac13
yinjiaqi <40558566+yinjiaqi@users.noreply.github.com>
2018-07-25 11:45:34 +0800

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const restService = express();
 const outside="I am outside ";
+const tableify = require('html-tableify');
 var mysql = require('mysql');
 
 
@@ -61,8 +62,29 @@ database: "sql12247448"
   if (err) throw err;
   con.query("SELECT * FROM Employee where eid="+qstring, function (err, result) {
     if (err) throw err;
-    var result2=JSON.stringify(result);
-    console.log(result2);
+tableify([{
+  eid: result.eid,
+  fname:result.fname,
+  lname: result.lname,
+  salary: result.salary
+}], {
+  headers: [{
+    name: 'eid',
+    align: 'left',
+    title: 'Employee id'
+  }, {
+    name: 'fname',
+    align: 'left'
+  }, {
+    name: 'lname',
+    align: 'left'
+  }, {
+    name: 'salary',
+    align: 'center'
+  }]
+});
+    
+    console.log(tableify);
   
     return res.json({
         fulfillmentText: speech,
@@ -79,7 +101,7 @@ database: "sql12247448"
                         {
                             "basicCard": {
                                 "title": outside + speech,
-                                "formattedText": " Your Query Input = "+ speech + "\n" +result2,
+                                "formattedText": " Your Query Input = "+ speech + "\n" +tableify,
                                 "image": {
                                     "url": "https://otb.cachefly.net/wp-content/uploads/2013/04/red-line.png",
                                     "accessibilityText": "Image alternate text"
